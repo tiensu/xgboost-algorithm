6eccb74e58f5a3f6cdab548ed45de35367b3cfa0
sunt <tiensunguyen2103@gmail.com>
2020-10-13 16:28:25 +0700

    tune subsample

---
+++ tune_subsample/tune_row_sample_rate.py
@@ -0,0 +1,37 @@
+# XGBoost on Otto dataset, tune subsample
+from pandas import read_csv
+from xgboost import XGBClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import LabelEncoder
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot
+# load data
+data = read_csv('train.csv')
+dataset = data.values
+# split data into X and y
+X = dataset[:,0:94]
+y = dataset[:,94]
+# encode string class values as integers
+label_encoded_y = LabelEncoder().fit_transform(y)
+# grid search
+model = XGBClassifier()
+subsample = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1.0]
+param_grid = dict(subsample=subsample)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+grid_search = GridSearchCV(model, param_grid, scoring="accuracy", n_jobs=-1, cv=kfold, verbose=1)
+grid_result = grid_search.fit(X, label_encoded_y)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+	print("%f (%f) with: %r" % (mean, stdev, param))
+# plot
+pyplot.errorbar(subsample, means, yerr=stds)
+pyplot.title("XGBoost subsample vs accuracy")
+pyplot.xlabel('subsample')
+pyplot.ylabel('accuracy')
+pyplot.savefig('subsample.png')
