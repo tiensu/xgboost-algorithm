548fb035696026f529d16ef95e43f6993995eb02
sunt <tiensunguyen2103@gmail.com>
2020-10-13 15:28:23 +0700

    tune learning_rate

---
+++ tune_learning_rate/tune_learning_rate_and_num_trees.py
@@ -0,0 +1,41 @@
+# XGBoost on Otto dataset, Tune learning_rate and n_estimators
+from pandas import read_csv
+from xgboost import XGBClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import LabelEncoder
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot
+import numpy
+# load data
+data = read_csv('train.csv')
+dataset = data.values
+# split data into X and y
+X = dataset[:,0:94]
+y = dataset[:,94]
+# encode string class values as integers
+label_encoded_y = LabelEncoder().fit_transform(y)
+# grid search
+model = XGBClassifier()
+n_estimators = [100, 200, 300, 400, 500]
+learning_rate = [0.0001, 0.001, 0.01, 0.1]
+param_grid = dict(learning_rate=learning_rate, n_estimators=n_estimators)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+grid_search = GridSearchCV(model, param_grid, scoring="accuracy", n_jobs=-1, cv=kfold)
+grid_result = grid_search.fit(X, label_encoded_y)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+	print("%f (%f) with: %r" % (mean, stdev, param))
+# plot results
+scores = numpy.array(means).reshape(len(learning_rate), len(n_estimators))
+for i, value in enumerate(learning_rate):
+    pyplot.plot(n_estimators, scores[i], label='learning_rate: ' + str(value))
+pyplot.legend()
+pyplot.xlabel('n_estimators')
+pyplot.ylabel('accuracy')
+pyplot.savefig('n_estimators_vs_learning_rate.png')
